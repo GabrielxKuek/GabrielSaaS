508f90b8d85dd39109381d724571024d0be3b395
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2025-05-06 22:10:43 +0800

    update dependencies to fix bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import TestPage from './pages/TestPage'
 import ErrorPage from './pages/ErrorPage'
-import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/test" element={<TestPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
